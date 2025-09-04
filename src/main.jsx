fb6c339078efb59f5b99137e0cd5dc083f9660f5
Aime Ishimwe <medicalmind07@gmail.com>
2025-09-04 15:02:45 +0200

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // use HashRouter for GitHub Pages
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-// Global CSS
+// Global CSS (load base styles first)
 import "./css/global.css";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
